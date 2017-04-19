a88d47e18862961e40fd106c20e1a585d6a6bc02
blessing philips <blessingadesina@gmail.com>
2017-04-19 11:39:43 +0100

    Added verification and authetication of user

---
+++ views/js/firebaseapp.js
@@ -0,0 +1,12 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBop_FLsc4JCzMrnlbm7ZKl8ox5CNP3I7g",
+    authDomain: "maintenancetracker-e4c77.firebaseapp.com",
+    databaseURL: "https://maintenancetracker-e4c77.firebaseio.com",
+    projectId: "maintenancetracker-e4c77",
+    storageBucket: "maintenancetracker-e4c77.appspot.com",
+    messagingSenderId: "305826009667"
+  };
+  firebase.initializeApp(config);
