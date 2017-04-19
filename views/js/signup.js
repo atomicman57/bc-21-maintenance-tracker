5441d145252a5d0e3374365bdbce90cd00ba758f
blessing philips <blessingadesina@gmail.com>
2017-04-19 13:36:50 +0100

    sign in authetication done

---
+++ views/js/signup.js
@@ -0,0 +1,59 @@
+function hidesignup(){
+	  $("#signupform").hide();
+	  }
+    
+	function verifymsg(){
+	var msg = "Your Registration on E-MTracker was successful, " 
+	var msg2 = "A verification mail have been sent to you, " 
+	var msg3 = "check your mail to verify your account. After Verification you can login"
+	  $("#verifiy").append(msg + msg2 + msg3);
+	  }
+	  
+	  
+  function signup(){
+ var Email = document.getElementById("email")
+ var Password = document.getElementById("password")
+ var fullName = document.getElementById("fullName").value
+ var username = document.getElementById("username").value
+ var email = Email.value;
+ var password = Password.value;
+var handleerror = "";
+ if(email.length > 5 && password.length > 3 && fullName.length > 5 && username.length > 4){
+ firebase.auth().createUserWithEmailAndPassword(email, password).then(function(value) {
+				firebase.auth().onAuthStateChanged(function(user) {
+				user.sendEmailVerification(); 
+				// if signup is sucessful hide signup
+				var uid = user.uid
+				hidesignup()
+				verifymsg()
+				firebase.database().ref('users').child(uid).set({
+				Fullname: fullName,
+				Username: username,
+				level: 1
+				 });
+				
+				
+    });
+                
+                }).catch(function(error) {
+                    var errorCode = error.code;
+					var errorMessage = error.message;
+					alert(errorMessage)
+                });
+
+
+ 
+
+
+ }
+else{
+
+	alert("Errors, check your input")
+}
+
+}
+ 
+
+
+
+ 
